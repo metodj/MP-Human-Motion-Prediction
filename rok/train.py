8bf5bf15aaba246bfc74e75b0984dc3b31540d25
Rok Šikonja <roksikonja5@gmail.com>
2019-04-24 18:22:14 +0200

    dim

--- rok/train.py
+++ rok/train.py
@@ -97,10 +97,10 @@ def create_model(session):
                                            extract_random_windows=True,
                                            num_parallel_calls=16)
         train_pl = train_data.get_tf_samples()
-        # print("train_pl:\t", str(type(train_pl)), "\t", "shape: " + str(tf.shape(train_pl)), "\t",
-        #       "get_shape: " + str(train_pl.get_shape()))
+
         print("train_pl\t", str(type(train_pl)))
-        # print("train_pl:\t", train_pl.get_shape().as_list())
+        print(train_pl.keys())
+        print(train_pl)
 
     # Load validation data.
     with tf.name_scope("validation_data"):
@@ -112,8 +112,9 @@ def create_model(session):
                                            extract_random_windows=False,
                                            num_parallel_calls=16)
         valid_pl = valid_data.get_tf_samples()
-        # print("valid_pl:\t", str(type(valid_pl)), "\t", "shape: " + str(tf.shape(valid_pl)), "\t",
-        #       "get_shape: " + str(valid_pl.get_shape()))
+        print("valid_pl\t", str(type(valid_pl)))
+        print(valid_pl.keys())
+        print(valid_pl)
 
     # Create the training model.
     with tf.name_scope(C.TRAIN):
@@ -138,6 +139,7 @@ def create_model(session):
     # Count and print the number of trainable parameters.
     num_param = 0
     for v in tf.trainable_variables():
+        print(v.name, str(v.get_shape()))
         num_param += np.prod(v.shape.as_list())
     print("# of parameters: " + str(num_param))
     config["num_parameters"] = int(num_param)
